827cc813ffdf503cabdf5450f44b9fdfc38bdc82
Natasha Isadora Kaweski Leite <natasha.leite@HOT-C02FG2VFMD6M.local>
2023-01-10 00:37:30 -0300

    Organazing buttons

--- src/Components/Application/index.js
+++ src/Components/Application/index.js
@@ -8,39 +8,62 @@ class Application extends React.Component {
         return (
             <div className='content'>
                 <div className="auto-cols-max flex flex-row content-start">
-                <div>01</div>
-                <div className='grow'>
-                    <div className='top'>
-                        <Link url='https://kawe.ski'/>
-                        <hr/>
+                    <div>
+                        <div className='bg-white'>
+                            <Button url='https://twitter.com/naweskil' icon={true} classes='button-white' />
+                            <Button url='https://twitter.com/naweskil' icon={true} classes='button-pink' />
+                            <Button url='https://twitter.com/naweskil' icon={true} classes='button-blue' />
+                            <Button url='https://twitter.com/naweskil' icon={true} classes='button-green' />
+                            <Button url='https://twitter.com/naweskil' icon={true} classes='button-gray' />
+                            <Button url='https://twitter.com/naweskil' icon={true} classes='button-yellow' />
+                        </div>
+                        <Button url='https://twitter.com/naweskil' icon={true} classes='button-white button-dark' />
+                        <Button url='https://twitter.com/naweskil' icon={true} classes='button-pink button-dark' />
+                        <Button url='https://twitter.com/naweskil' icon={true} classes='button-blue button-dark' />
+                        <Button url='https://twitter.com/naweskil' icon={true} classes='button-green button-dark' />
+                        <Button url='https://twitter.com/naweskil' icon={true} classes='button-gray button-dark' />
+                        <Button url='https://twitter.com/naweskil' icon={true} classes='button-yellow button-dark' />
                     </div>
-                    <div className='box flex items-stretch'>
-                        <div className='max-w-0 photo'></div>
-                        <div>
-                            <span className='tag'>about me</span>
-                            <h1 className='title white'>Ex front-end developer, actual Design System Ops Senior, The Sims 4 and Minecraft player</h1>
-                            <span className='text gray'>Today I am working on Cosmos Design System</span>
-                            <div className='box-footer flex flex-row items-center'>
-                                <div className='mr-5'><span className='avatar'></span></div>
-                                <div className='text'>
-                                    <span className='title black'>Instagram</span>
-                                    <span className='text black'>@naweskil</span>
-                                </div>
-                                <div className='text'>
-                                    <Button url='#' content={false} classes='go-link' icon={true} />
+                    <div className='grow'>
+                        <div className='top'>
+                            <Link url='https://kawe.ski'/>
+                            <hr/>
+                        </div>
+                        <div className='box flex items-stretch'>
+                            <div className='max-w-0 photo'></div>
+                            <div>
+                                <span className='tag'>about me</span>
+                                <h1 className='title white'>Ex front-end developer, actual Design System Ops Senior, The Sims 4 and Minecraft player</h1>
+                                <span className='text gray'>Today I am working on Cosmos Design System</span>
+                                <div className='box-footer flex flex-row items-center'>
+                                    <div className='mr-5'><span className='avatar'></span></div>
+                                    <div className='text'>
+                                        <span className='title black'>Instagram</span>
+                                        <span className='text black'>@naweskil</span>
+                                    </div>
+                                    <div className='text'>
+                                        <Button url='#' classes='go-link' icon={true} />
+                                    </div>
                                 </div>
                             </div>
                         </div>
                     </div>
-                </div>
-                <div>
-                    <Button url='https://twitter.com/naweskil' content='Twitter' />
-                    <Button url='https://twitter.com/naweskil' content='Twitter' classes='button-pink' />
-                    <Button url='https://twitter.com/naweskil' content='Twitter' classes='button-blue' />
-                    <Button url='https://twitter.com/naweskil' content='Twitter' classes='button-green' />
-                    <Button url='https://twitter.com/naweskil' content='Twitter' classes='button-gray' />
-                    <Button url='https://twitter.com/naweskil' content='Twitter' classes='button-yellow' />
-                </div>
+                    <div>
+                        <div className='bg-white pad2'>
+                            <Button url='https://twitter.com/naweskil' content='Twitter' classes='button-white' />
+                            <Button url='https://twitter.com/naweskil' content='Twitter' classes='button-pink' />
+                            <Button url='https://twitter.com/naweskil' content='Twitter' classes='button-blue' />
+                            <Button url='https://twitter.com/naweskil' content='Twitter' classes='button-green' />
+                            <Button url='https://twitter.com/naweskil' content='Twitter' classes='button-gray' />
+                            <Button url='https://twitter.com/naweskil' content='Twitter' classes='button-yellow' />
+                        </div>
+                        <Button url='https://twitter.com/naweskil' content='Twitter' classes='button-white button-dark' />
+                        <Button url='https://twitter.com/naweskil' content='Twitter' classes='button-pink button-dark' />
+                        <Button url='https://twitter.com/naweskil' content='Twitter' classes='button-blue button-dark' />
+                        <Button url='https://twitter.com/naweskil' content='Twitter' classes='button-green button-dark' />
+                        <Button url='https://twitter.com/naweskil' content='Twitter' classes='button-gray button-dark' />
+                        <Button url='https://twitter.com/naweskil' content='Twitter' classes='button-yellow button-dark' />
+                    </div>
                 </div>
             </div>
         );
